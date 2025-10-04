89afe537f7e279d6339feeb8b5f78c67bf7a0e68
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-10-04 11:11:06 +0600

    Added

--- crems/lib/main.dart
+++ crems/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SignIn(),
+      title: "CREMS - Corporate Real Estate Management System"
     );
   }
 }
