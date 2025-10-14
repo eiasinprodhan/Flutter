5b99beff2e99cc3fee98169a2d95ed777eb117dc
Eiasin Prodhan <eiasinprodhan@gmail.com>
2025-10-14 19:25:44 +0600

    Added

--- crems/lib/main.dart
+++ crems/lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Real Estate Management',
+      title: 'CREMS',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
